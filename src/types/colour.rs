5ca93d50f7c98c2ab5389c9931eaaf640188e15f
Piotr Wach <pwach@bloomberg.net>
2023-11-07 21:15:46 +0000

    Colour tests

--- src/types/colour.rs
+++ src/types/colour.rs
@@ -27,3 +27,20 @@ pub struct ColourStop {
     pub position: f32,
     pub color: Colour,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_transparent() {
+        assert!(Colour { a: 0.0, r: 0.0, g: 0.0, b: 0.0 }.is_transparent());
+        assert!(!Colour { a: 1.0, r: 0.0, g: 0.0, b: 0.0 }.is_transparent());
+    }
+
+    #[test]
+    fn rgba() {
+        assert_eq!(Colour { a: 0.0, r: 0.0, g: 0.0, b: 0.0 }.rgba(), "rgba(0,0,0,0)");
+        assert_eq!(Colour { a: 0.5, r: 0.1, g: 0.2, b: 0.3 }.rgba(), "rgba(25.5,51,76.5,0.5)");
+    }
+}
